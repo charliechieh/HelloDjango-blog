04469fb34f5a89a2424bf769fbaa637b220277cb
charlie <easmartss@gmail.com>
2020-09-09 08:52:52 +0800

    style: 修改部分样式

--- comments/forms.py
+++ comments/forms.py
@@ -6,3 +6,10 @@ class CommentFrom(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'url', 'text']
+
+        widgets = {
+            'name': forms.widgets.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.widgets.TextInput(attrs={'class': 'form-control'}),
+            'url': forms.widgets.TextInput(attrs={'class': 'form-control'}),
+            'text': forms.widgets.Textarea(attrs={'class': 'form-control', 'rows': "4"})
+        }
